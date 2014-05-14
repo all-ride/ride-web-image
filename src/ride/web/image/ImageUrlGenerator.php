b983871eb04511f0c64588d1cb8bf2efd1c8f4e6
Joris Vandeweerd <joris@statik.be>
2014-05-14 14:17:59 +0200

    reworked ImageUrlGenerator to allow external URLs

--- src/ride/web/image/ImageUrlGenerator.php
+++ src/ride/web/image/ImageUrlGenerator.php
@@ -3,6 +3,7 @@
 namespace ride\web\image;
 
 use ride\library\dependency\DependencyInjector;
+use ride\library\http\Response;
 use ride\library\image\exception\ImageException;
 use ride\library\image\Dimension;
 use ride\library\image\ImageUrlGenerator as LibImageUrlGenerator;
@@ -39,6 +40,12 @@ class ImageUrlGenerator implements LibImageUrlGenerator {
     private $baseUrl;
 
     /**
+     * Absolute path of the public directory
+     * @var string
+     */
+    private $publicPath;
+
+    /**
      * The path for the processed images
      * @var ride\library\filesystem\File
      */
@@ -55,6 +62,7 @@ class ImageUrlGenerator implements LibImageUrlGenerator {
         $this->fileBrowser = $fileBrowser;
         $this->dependencyInjector = $dependencyInjector;
         $this->baseUrl = $baseUrl;
+        $this->publicPath = $fileBrowser->getPublicDirectory()->getAbsolutePath();
 
         $this->setPath($path);
     }
@@ -90,90 +98,130 @@ class ImageUrlGenerator implements LibImageUrlGenerator {
      * @return null
      */
     public function generateUrl($image, $thumbnailer = null, $width = 0, $height = 0) {
-        // ignore urls
-        if (strlen($image) > 7 && substr($image, 0, 7) == 'http://' || substr($image, 0, 8) == 'https://') {
-            return $image;
-        }
-
-        $fileSource = $this->fileBrowser->getFileSystem()->getFile($image);
-
-        if (!$fileSource->isAbsolute()) {
-            // relative file, get the image from the file browser
-            $fileSource = $this->fileBrowser->getPublicFile($image);
-            if (!$fileSource) {
-                $fileSource = $this->fileBrowser->getFile($image);
-            }
-
-            if (!$fileSource) {
-                throw new ImageException('Could not generate URL for ' . $image . ': file not found');
-            }
-        }
-
-        $publicDirectory = $this->fileBrowser->getPublicDirectory()->getAbsolutePath();
         $useThumbnailer = $thumbnailer && ($width > 0 || $height > 0);
-        $isPublicFile = strpos($fileSource->getAbsolutePath(), $publicDirectory) === 0;
 
-        if (!$useThumbnailer && $isPublicFile) {
-            // no processing needed, use source file since it's public
-            $fileDestination = $fileSource;
+        if (strlen($image) > 7 && substr($image, 0, 7) == 'http://' || substr($image, 0, 8) == 'https://') {
+            // image is a URL
+            if (!$useThumbnailer) {
+                return $image;
+            }
+
+            $file = $this->getCacheFile($image, $thumbnailer, $width, $height);
+            if (!$file->exists()) {
+                $httpClient = $this->dependencyInjector->get('ride\\library\\http\\client\\Client');
+                $response = $httpClient->get($image);
+                if ($response->getStatusCode() != Response::STATUS_CODE_OK) {
+                    throw new ImageException('Could not generate URL for ' . $image . ': file not found');
+                }
+
+                $file->write($response->getBody());
+
+                $this->applyThumbnailer($file, $thumbnailer, $width, $height);
+            }
         } else {
-            // processing needed
-            $fileDestination = $this->getCacheFile($fileSource, $thumbnailer, $width, $height);
+            // image is a local file
+            $source = $this->lookupFile($image);
 
-            if (!$fileDestination->exists() || $fileSource->getModificationTime() > $fileDestination->getModificationTime()) {
-                // needed file does not exist or has been changed
-                if ($useThumbnailer) {
-                    $thumbnailer = $this->dependencyInjector->get('ride\\library\\image\\thumbnail\\Thumbnailer', $thumbnailer);
-                    $imageFactory = $this->dependencyInjector->get('ride\\library\\image\\io\\ImageFactory');
+            $isPublicFile = strpos($source->getAbsolutePath(), $this->publicPath) === 0;
+            if (!$useThumbnailer && $isPublicFile) {
+                $file = $source;
+            } else {
+                $file = $this->getCacheFile($source->getPath(), $thumbnailer, $width, $height);
 
-                    $image = $imageFactory->read($fileSource);
+                if (!$file->exists() || $source->getModificationTime() > $file->getModificationTime()) {
+                    $source->copy($file);
 
-                    $thumbnail = $thumbnailer->getThumbnail($image, new Dimension($width, $height));
-                    if ($image !== $thumbnail) {
-                        // thumbnail generated, write to cache file
-                        $imageFactory->write($fileDestination, $thumbnail);
-                    } elseif ($isPublicFile) {
-                        // no processing done and a public file, use source file since it's public
-                        $fileDestination = $fileSource;
-                    } else {
-                        // no processing done and a non-public file, copy to the cache file
-                        $fileSource->copy($fileDestination);
+                    if ($useThumbnailer) {
+                        $this->applyThumbnailer($file, $thumbnailer, $width, $height);
                     }
-                } else {
-                    // non-public file, copy to the cache file
-                    $fileSource->copy($fileDestination);
                 }
             }
         }
 
-        // make the image relative to the public directory
-        $image = str_replace($publicDirectory, '', $fileDestination->getAbsolutePath());
+        // make the resulting image relative to the public directory
+        $image = str_replace($this->publicPath, '', $file->getAbsolutePath());
 
         // return the full URL
         return $this->baseUrl . $image;
     }
 
     /**
+     * Applies the provided thumbnailer to the provided file
+     * @param \ride\library\system\file\File $file File of the source image
+     * @param string $thumbnailer Name of the thumbnailer
+     * @param integer $width Width of the resulting image
+     * @param integer $height Height of the resulting image
+     * @return null
+     */
+    protected function applyThumbnailer(File $file, $thumbnailer, $width, $height) {
+        $thumbnailer = $this->dependencyInjector->get('ride\\library\\image\\thumbnail\\Thumbnailer', $thumbnailer);
+        $imageFactory = $this->dependencyInjector->get('ride\\library\\image\\io\\ImageFactory');
+
+        $image = $imageFactory->read($file);
+
+        $thumbnail = $thumbnailer->getThumbnail($image, new Dimension($width, $height));
+        if ($image !== $thumbnail) {
+            // thumbnail generated, write to cache file
+            $imageFactory->write($file, $thumbnail);
+        }
+    }
+
+    /**
      * Gets the cache file for the image source
-     * @param ride\library\system\file\File $source image source to get a
-     * cache file for
+     * @param string $source Source to get a cache file for (local path or URL)
      * @param string $thumbnailer Name of the thumbnailer
      * @param integer $width The width of the cached image
      * @param integer $height The height of the cached image
      * @return ride\library\system\file\File unique name for a source file, in
      * the cache directory, with the thumbnailer, width and height encoded into
      */
-    private function getCacheFile(File $source, $thumbnailer = null, $width = 0, $height = 0) {
-        $filename = md5(
-            $source->getPath() .
+    protected function getCacheFile($source, $thumbnailer = null, $width = 0, $height = 0) {
+        $fileName = md5(
+            $source .
             '-thumbnailer=' . $thumbnailer .
             '-width=' . $width .
             '-height=' . $height
         );
 
-        $filename .= '-' . $source->getName();
+        $positionSlash = strrpos($source, '/');
+        if ($positionSlash !== false) {
+            $name = substr($source, $positionSlash + 1);
+        } else {
+            $name = $source;
+        }
 
-        return $this->path->getChild($filename);
+        $fileName .= '-' . $name;
+
+        return $this->path->getChild($fileName);
+    }
+
+    /**
+     * Gets the local file for the provided file path
+     * @param string $source Path of a local file
+     * @return \ride\library\system\file\File
+     * @throws \ride\library\image\exception\ImageException when the file could
+     * not be found
+     */
+    protected function lookupFile($source) {
+        // image is a local file
+        $file = $this->fileBrowser->getFileSystem()->getFile($source);
+
+        if ($file->isAbsolute()) {
+            return $file;
+        }
+
+        // relative file, get the image from the file browser
+        $file = $this->fileBrowser->getPublicFile($source);
+        if ($file) {
+            return $file;
+        }
+
+        $file = $this->fileBrowser->getFile($source);
+        if ($file) {
+            return $file;
+        }
+
+        throw new ImageException('Could not generate URL for ' . $source . ': file not found');
     }
 
 }
